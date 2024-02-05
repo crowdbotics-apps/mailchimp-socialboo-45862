b355c69e32c7eadd7f064d5f71fe15790d985b88
Crowdbotics <team@crowdbotics.com>
2024-02-05 04:31:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mailchimp-socialboo-45862",
+  url: "https://mailchimp-socialboo-45862.botics.co",
+  api: "https://mailchimp-socialboo-45862.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
